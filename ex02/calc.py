13df5971e8bcddefa4048f41bf7a915cfb4d6208
c0b2115312 <c0b2115312@edu.teu.ac.jp>
2022-10-04 15:13:15 +0900

    ウィンドウ作成練習5

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,18 +1,24 @@
+import numbers
 import tkinter as tk
 import tkinter.messagebox as tkm
 
 def button_click(event):
     btn = event.widget
-    num = int(btn["text"])
-    tkm.showinfo(f"{num}",f"{num}ボタンが押されました")
+    num = btn["text"]
+    #tkm.showinfo(f"{num}",f"{num}ボタンが押されました")
+    entry.insert(tk.END,num)
 
 root = tk.Tk()
 root.title("練習問題1")
 root.geometry("300x500")
 
-r,c = 0,0
+entry = tk.Entry(root, width=10, font=(", 40"), justify="right") # 練習4
+entry.grid(row=0, column=0, columnspan=3)
 
-for i,num in enumerate(range(9,-1,-1),1):
+r,c = 1,0
+numbers = list(range(9,-1,-1))
+operators = ["+"]
+for i,num in enumerate(numbers+operators,1):
     button = tk.Button(root, text=f"{num}",font=("",30),width="4",height="2")
     button.bind("<1>",button_click)
     button.grid(row=r, column=c)
